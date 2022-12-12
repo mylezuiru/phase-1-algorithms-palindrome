5fe62011e2812356c384c944198c895c3e9df17a
mylezuiru <mylesuiru@gmail.com>
2022-12-12 10:59:35 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  // Write your algorithm here  
+  const splittedWord = word.split("")
+  const reversedArray = splittedWord.reverse()
+  const reversedWord = reversedArray.join("")
+
+  if(word.toLowerCase() === reversedWord.toLowerCase()){
+    return true
+  }else{
+    return false
+  }
+
 }
 
 /* 
   Add your pseudocode here
-*/
+Look if the word is equal to the reversed list
+  If the reversed list is not the same as the word  it returns false
+  If the reversed list is the same as the word  it returns true
 
+*/
 /*
   Add written explanation of your solution here
-*/
+  first: change the  word received to  reversed list
+  second: ensure the word received and reversed list are the same
+  third: it will return true or false
+  */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
